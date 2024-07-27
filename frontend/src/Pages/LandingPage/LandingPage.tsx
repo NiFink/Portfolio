077e5014617a4dee6fa20471b872970b7fd55e86
NiFink <Nils@team-fink.de>
2024-07-27 21:32:01 +0200

    Add: Carousel to LandingPage

--- frontend/src/Pages/LandingPage/LandingPage.tsx
+++ frontend/src/Pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import PortfolioPage from '../PortfolioPage/LandingPage';
+import React from "react";
+import PortfolioPage from "../PortfolioPage/LandingPage";
+import LandingCarousel from "./Components/LandingCarousel";
 
 function LandingPage() {
   return (
@@ -7,15 +8,23 @@ function LandingPage() {
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <div className="bg-white col-span-2 h-80 rounded-lg"></div>
         <div className="row-span-2  h-full rounded-lg">
-        <div className='grid grid-rows-8 h-full gap-4'>
-                <div className='row-span-5 bg-white rounded-lg '></div>
-                <div className='row-span-3 bg-white rounded-lg '></div>
-            </div>
+          <div className="grid grid-rows-8 h-full gap-4">
+            <div className="row-span-5 bg-white rounded-lg "></div>
+            <div className="row-span-3 bg-white rounded-lg "></div>
+          </div>
         </div>
-        
-        <div className= "bg-white row-span-2  h-full rounded-lg"></div>
-        <div className="bg-slate-500 aspect-square rounded-lg text-center"> Profile Picture</div>
-        <div className="bg-slate-500 aspect-square rounded-lg text-center"> Contact </div>
+
+        <div className="bg-white row-span-2  h-full rounded-lg">
+          <LandingCarousel/>
+        </div>
+        <div className="bg-slate-500 aspect-square rounded-lg text-center">
+          {" "}
+          Profile Picture
+        </div>
+        <div className="bg-slate-500 aspect-square rounded-lg text-center">
+          {" "}
+          Contact{" "}
+        </div>
       </div>
     </div>
   );
