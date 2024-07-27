d3694c076a1a046093e843f447afc25eb73483ca
NiFink <Nils@team-fink.de>
2024-07-27 23:21:34 +0200

    Update: Carousel and Contacts

--- frontend/src/Pages/LandingPage/LandingPage.tsx
+++ frontend/src/Pages/LandingPage/LandingPage.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 import PortfolioPage from "../PortfolioPage/LandingPage";
 import LandingCarousel from "./Components/LandingCarousel";
+import Contact from "./Components/Contact";
 
 function LandingPage() {
   return (
-    <div className="px-16 pt-16 ">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div className="bg-white col-span-2 h-80 rounded-lg"></div>
+    <div className="px-16 py-16 ">
+      <div className="grid grid-cols-1  grid-rows-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
+        <div className="bg-green-800  col-span-2 rows-span-3 rounded-lg"></div>
         <div className="row-span-2  h-full rounded-lg">
-          <div className="grid grid-rows-8 h-full gap-4">
-            <div className="row-span-5 bg-white rounded-lg "></div>
+          <div className="grid grid-rows-8 h-full gap-7">
+            <div className="row-span-5 bg-white rounded-lg ">
+              <img
+                src="./Pictures/city.jpg"
+                alt=""
+                className="h-full w-full object-cover rounded-lg"
+                style={{ objectFit: "cover" }}
+              />
+            </div>
             <div className="row-span-3 bg-white rounded-lg "></div>
           </div>
         </div>
 
         <div className="bg-white row-span-2  h-full rounded-lg">
-          <LandingCarousel/>
+          <LandingCarousel />
         </div>
-        <div className="bg-slate-500 aspect-square rounded-lg text-center">
+        <div className="bg-white   rounded-lg text-center">
           {" "}
           Profile Picture
         </div>
-        <div className="bg-slate-500 aspect-square rounded-lg text-center">
-          {" "}
-          Contact{" "}
+        <div className="rounded-lg text-center">
+          <Contact />
         </div>
       </div>
     </div>
