8e2188bc2794bf22c7cc2ccd5175be4593d3049f
NiFink <Nils@team-fink.de>
2024-07-29 20:24:16 +0200

    Update: Structure, Add: Pictures

--- frontend/src/Pages/LandingPage/Components/Contact.tsx
+++ frontend/src/Pages/LandingPage/Components/Contact.tsx
@@ -1,18 +1,18 @@
 function Contact() {
   return (
-    <div className="flex h-full rounded-lg items-center justify-center">
+    <div className="flex h-full">
       {/* Icons go here */}
-      <div className="grid grid-cols-2 w-full h-full text-3xl md:text-5xl lg:text-7xl text-white gap-6 ">
-        <button className="focus:outline-none aspect-square hover:text-gray-400 rounded-lg bg-gradient-to-br from-fuchsia-900 to-pink-600">
+      <div className="grid grid-cols-2 w-full h-full text-3xl md:text-5xl lg:text-7xl text-white gap-4 ">
+        <button className=" h-full w-full  hover:text-gray-400 rounded-lg bg-gradient-to-br from-fuchsia-900 to-pink-600">
           <i className="bi bi-instagram"></i>
         </button>
-        <button className="focus:outline-none aspect-square hover:text-gray-400 rounded-lg bg-gradient-to-bl from-indigo-900 to-blue-700 ">
+        <button className="h-full  hover:text-gray-400 rounded-lg bg-gradient-to-bl from-indigo-900 to-blue-700 ">
           <i className="bi bi-linkedin "></i>
         </button>
-        <button className="focus:outline-none aspect-square hover:text-gray-400 rounded-lg bg-gradient-to-tr to-violet-600 from-indigo-800">
+        <button className="h-full  hover:text-gray-400 rounded-lg bg-gradient-to-tr to-violet-600 from-indigo-800">
           <i className="bi bi-github "></i>
         </button>
-        <button className="focus:outline-none aspect-square hover:text-gray-400 rounded-lg bg-gradient-to-tl to-emerald-700 from-emerald-900">
+        <button className="h-full  hover:text-gray-400 rounded-lg bg-gradient-to-tl to-emerald-700 from-emerald-900">
           <i className="bi bi-envelope-at-fill"></i>
         </button>
       </div>
