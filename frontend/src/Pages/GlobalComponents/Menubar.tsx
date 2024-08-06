ca20cb6ae18707b6b671a13d10099a3a5365c0a0
NiFink <Nils@team-fink.de>
2024-08-06 21:47:53 +0200

    Update; landingCarousel

--- frontend/src/Pages/GlobalComponents/Menubar.tsx
+++ frontend/src/Pages/GlobalComponents/Menubar.tsx
@@ -1,16 +1,54 @@
 interface MenubarProps {
   landingPageClick: () => void;
-  portfolioPageClick: () => void;
+  projectsPageClick: () => void;
+  aboutmePageClick: () => void;
+  currentPage: string;
 }
 
 function Menubar({
   landingPageClick,
-  portfolioPageClick,
+  projectsPageClick,
+  aboutmePageClick,
+  currentPage,
 }: MenubarProps) {
+  const getBackgroundClass = (page: string) => {
+    return currentPage === page ? "bg-indigo-200" : "bg-white"; // Highlight the active page with a different background color
+  };
 
   return (
-    <div className="z-10 w-full">
-      
+    <div className="z-10 w-full p-5 flex justify-center fixed bg-slate-900 ">
+      <div
+        className={`mr-10 ${getBackgroundClass(
+          "LandingPage"
+        )} h-14 w-32 flex justify-center items-center rounded-lg cursor-pointer font-bold text-indigo-950 text-2xl hover:bg-indigo-400 hover:text-white`}
+        onClick={landingPageClick}
+      >
+        Home
+      </div>
+      <div
+        className={`mr-10 ${getBackgroundClass(
+          "AboutMePage"
+        )} h-14 w-32 flex justify-center items-center rounded-lg cursor-pointer font-bold text-indigo-950 text-2xl hover:bg-indigo-400 hover:text-white`}
+        onClick={aboutmePageClick}
+      >
+        About Me
+      </div>
+      <div
+        className={`mr-10 ${getBackgroundClass(
+          "ProjectsPage"
+        )} h-14 w-32 flex justify-center items-center rounded-lg cursor-pointer font-bold text-indigo-950 text-2xl hover:bg-indigo-400 hover:text-white`}
+        onClick={projectsPageClick}
+      >
+        Projects
+      </div>
+      <div
+        className={` ${getBackgroundClass(
+          "Contact"
+        )} h-14 w-32 flex justify-center items-center rounded-lg cursor-pointer font-bold text-indigo-950 text-2xl hover:bg-indigo-400 hover:text-white`}
+        onClick={landingPageClick}
+      >
+        Contact
+      </div>
     </div>
   );
 }
--- frontend/src/Pages/ProjectsPage/ProjectsPage.tsx
+++ frontend/src/Pages/ProjectsPage/ProjectsPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function ProjectsPage() {
+interface ProjectsPageProps {
+  LandingPageClick: () => void;
+}
+
+function ProjectsPage({LandingPageClick}: ProjectsPageProps) {
   return (
-    <div></div>
+    <div className='h-screen bg-gradient-to-tl from-neutral-900 via-indigo-900 to-slate-900'>
+
+    </div>
   );
 }
 
