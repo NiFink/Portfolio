5e302ab43d7bc2458cc7a53d5f615908b8f07700
NiFink <Nils@team-fink.de>
2024-08-16 15:40:13 +0200

    update: filestructure

--- frontend/src/Pages/LandingPage/Components/LandingText.tsx
+++ frontend/src/Pages/LandingPage/Components/LandingText.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function LandingText() {
   return (
-    <div className="h-full  p-10 bg-gradient-to-t from-rose-100 to-orange-300 bg-clip-text text-transparent overflow-hidden hover:overflow-scroll">
-      <h1 className="text-4xl md:text-5xl lg:text-6xl mb-4 font-bold font-mono ">
+    <div className="h-full  p-10 bg-gradient-to-t from-white to-orange-300 bg-clip-text text-transparent overflow-hidden hover:overflow-scroll " >
+      <h1 className="text-4xl md:text-5xl lg:text-5xl mb-4 font-bold font-mono ">
         Welcome to My Programming Portfolio!
       </h1>
-      <div className="text-base md:text-lg lg:text-xl leading-relaxed ">
+      <div className="text-base md:text-lg lg:text-2xl leading-relaxed font-semibold">
         <p className='mt-14'>
           Here, you'll find a curated selection of my projects and experiences that showcase my journey and expertise in software development.
         </p>
