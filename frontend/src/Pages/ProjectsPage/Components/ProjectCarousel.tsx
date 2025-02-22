d8fdcbf035c05576ce47b23b294d4a678ae43dc9
NiFink <Nils@team-fink.de>
2025-02-22 16:05:00 +0100

    finish

--- frontend/src/Pages/ProjectsPage/Components/ProjectCarousel.tsx
+++ frontend/src/Pages/ProjectsPage/Components/ProjectCarousel.tsx
@@ -212,7 +212,7 @@ function ProjectCarousel() {
         ))}
       </div>
 
-      <div className="absolute bottom-0 flex justify-center gap-5 w-full h-xl:pb-4 ">
+      <div className="absolute bottom-0 flex justify-center gap-5 w-full pb-4 h-xl:pb-4 ">
         <div className="flex  gap-5 py-6 px-8 bg-white rounded-full shadow-2xl">
           {slides.map((_, i) => (
             <div
