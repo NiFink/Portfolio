9933de2d60462026145547ce54962fee73a1a751
NiFink <Nils@team-fink.de>
2025-03-22 14:08:37 +0100

    Update: data

--- frontend/src/Pages/AboutMePage/AboutMePage.tsx
+++ frontend/src/Pages/AboutMePage/AboutMePage.tsx
@@ -4,7 +4,7 @@ function AboutMePage() {
   return (
     <div className="h-full pt-20 justify-center bg-gradient-to-b from-indigo-200  to-white">
       <div className="w-full text-center text-5xl font-bold  text-slate-800 mt-10">
-        Here you can see my life story 😎
+        Here you can see my timeline 😎
       </div>
       <Timeline defaultColor="bg-white"/>
     </div>
